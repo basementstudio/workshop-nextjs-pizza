ab1f24e3f51c4cc0abef48c6cb2d0c0357850dcd
Agu <agugalgui@gmail.com>
2023-04-12 21:10:43 +0200

    cart addded

---
+++ src/app/components/cart.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+
+import CartCloseButton from './cart-btn-close'
+import CartFooter from './cart-footer'
+import CartHeader from './cart-header'
+import CartProduct from './cart-product'
+
+export default function Cart() {
+  return (
+    <div className="fixed top-0 z-50 h-screen w-screen bg-black/50">
+      <div className="min-w-xl absolute right-0  h-screen  rounded-bl-3xl border-l-2 border-black bg-cream">
+        <CartHeader />
+        <div className="mx-12 mt-12 flex flex-col gap-6">
+          <CartProduct />
+          <CartProduct />
+        </div>
+        <CartFooter />
+      </div>
+    </div>
+  )
+}
