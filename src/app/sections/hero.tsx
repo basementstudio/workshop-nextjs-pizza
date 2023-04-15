3dbffe6e1d7085234e05ee20afa5842999f0b915
Agu <agugalgui@gmail.com>
2023-04-15 18:20:34 +0200

    feat: mobile hero

--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
-    <section className="bg-cream pb-24 pt-48">
-      <h1 className="relative text-center">
-        <span className="font-display text-hero font-black leading-trim tracking-[-.02em] text-black">
+    <section className="bg-cream pb-9 pt-24 md:pb-12 md:pt-48">
+      <h1 className="relative w-full text-center">
+        <span className="relative block text-center font-display text-[16vw] font-black leading-trim text-black">
           SPICY SHOP FT.
         </span>
-        <span className="absolute -bottom-[40px] left-1/2 -translate-x-1/2 transform whitespace-nowrap text-center font-neon text-9xl text-pink">
+        <span className="relative top-[-36px] block text-center font-neon text-5xl text-pink sm:-top-8 sm:text-6xl md:-top-12 md:text-8xl lg:top-[-96px] lg:text-9xl xl:text-9xl">
           Julian Benegas
         </span>
       </h1>
