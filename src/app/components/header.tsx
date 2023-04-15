eba1a490c61edf530800a6093de55244c53f2834
Agu <agugalgui@gmail.com>
2023-04-15 18:21:32 +0200

    feat: mobile header nav

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,9 +3,9 @@ import Logo from './logo'
 
 export default function Header() {
   return (
-    <header className=" fixed top-0 z-50 min-w-full bg-cream">
+    <header className="fixed top-0 z-50 min-w-full bg-cream">
       <div className="m-auto max-w-7xl">
-        <nav className="  flex h-24 w-full max-w-7xl items-center justify-between bg-cream font-display text-base font-bold text-black">
+        <nav className="mx-4 flex h-16 max-w-7xl items-center justify-between bg-cream font-display text-base font-bold text-black sm:mx-8 sm:h-24 md:mx-16 lg:mx-12 xl:mx-auto">
           <Logo />
           <div className="hidden gap-8 py-2 lg:flex">
             <div>HOME</div>
