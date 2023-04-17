3e9ce069c66d6d38c3147d03af83e849131c711f
Agu <agugalgui@gmail.com>
2023-04-17 21:10:38 +0200

    feat: ellipse component radius

---
+++ src/app/components/primitives/ellipse.tsx
@@ -0,0 +1,9 @@
+const Ellipse = () => {
+  return (
+    <div className="ellipse h-[64px] w-[144px] items-center justify-center border-2 border-black bg-cream py-3 text-2xl font-black text-pink">
+      PRICE
+    </div>
+  )
+}
+
+export default Ellipse
