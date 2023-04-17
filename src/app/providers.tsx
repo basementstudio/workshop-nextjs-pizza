0cb6f44a745002dc86c13437550cc7617df8fed5
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2023-04-17 11:43:38 -0300

    fix: add user is tabbing hook

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,29 @@
 'use client'
 
+import { useEffect } from 'react'
 import { QueryClientProvider } from '~/shopify/storefront-hooks'
 
 export const Providers = ({ children }: { children?: React.ReactNode }) => {
+
+  // User is tabbing hook
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.code === `Tab`) {
+        document.body.classList.add('user-is-tabbing')
+      }
+    }
+
+    function handleMouseDown() {
+      document.body.classList.remove('user-is-tabbing')
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    window.addEventListener('mousedown', handleMouseDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+      window.removeEventListener('mousedown', handleMouseDown)
+    }
+  }, [])
+
   return <QueryClientProvider>{children}</QueryClientProvider>
 }
