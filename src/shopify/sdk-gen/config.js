692e74cd732db122a345c789d846d241581b3e74
Julian Benegas <julianbenegas99@gmail.com>
2023-04-18 08:54:54 -0400

    add env example

--- src/shopify/sdk-gen/config.js
+++ src/shopify/sdk-gen/config.js
@@ -4,6 +4,7 @@
 module.exports = {
   endpoint: 'https://next-js-pizza-by-bsmnt.myshopify.com/api/2023-01/graphql',
   headers: {
-    'x-shopify-storefront-access-token': 'ec7a93b2117db2883fdda0af0c50cd2b'
+    'x-shopify-storefront-access-token':
+      process.env.NEXT_PUBLIC_STOREFRONT_ACCESS_TOKEN
   }
 }
